12a8463c9d76f4022b1fa4e1e6e0dc4f2c2fce4f
Austin Delorme <austin@funraise.io>
2017-06-26 09:30:42 -0700

    Pull onSuccess and onFailure up to ProfileCallback from Basic/FullProfileCallback, add generic typing

--- src/main/java/io/funraise/requests/callbacks/BasicProfileCallback.java
+++ src/main/java/io/funraise/requests/callbacks/BasicProfileCallback.java
@@ -1,32 +1,14 @@
 package io.funraise.requests.callbacks;
 
 import io.funraise.models.BasicProfile;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by jason on 6/25/17.
  */
-public class BasicProfileCallback extends ProfileCallback implements Callback<BasicProfile> {
+public class BasicProfileCallback extends ProfileCallback<BasicProfile> {
 
     public BasicProfileCallback(ProfileCallbackDelegate delegate) {
         super(delegate);
     }
 
-    @Override
-    public void onResponse(Call<BasicProfile> call, Response<BasicProfile> response) {
-        if (response.isSuccessful()) {
-            profileResponse.profile = response.body();
-        } else {
-            profileResponse.error = response.message().toString();
-        }
-        onComplete();
-    }
-
-    @Override
-    public void onFailure(Call<BasicProfile> call, Throwable throwable) {
-        profileResponse.error = throwable.getMessage();
-        onComplete();
-    }
 }
